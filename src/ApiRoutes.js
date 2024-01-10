c7c9758ba743b20fe0d8cba3bbcd10123b785d55
AbhishekNayak01 <abhishek.nayak@saarthi.ai>
2024-01-10 16:29:21 +0530

    api route

--- src/ApiRoutes.js
+++ src/ApiRoutes.js
@@ -1,5 +1,6 @@
 export const urls = {
     GET_CHARACTERS: "https://rickandmortyapi.com/api/character",
     GET_LOCATIONS: "https://rickandmortyapi.com/api/location",
-    GET_EPICODES: "https://rickandmortyapi.com/api/episode"
+    GET_EPICODES: "https://rickandmortyapi.com/api/episode",
+   
 }
